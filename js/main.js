c2b195419ab2bed7fdf8726141837015791263c5
AHCG <Applied.Hydroclimatology@dartmouth.edu>
2021-10-20 00:58:46 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -7,21 +7,21 @@ $(document).ready(function(){
 	$("#testimonial-slider").owlCarousel({
 	    paginationSpeed : 500,      
 	    singleItem:true,
-	    autoPlay: 70000,
+	    autoPlay: 50000,
 	});
 
 
 
 
 	$("#clients-logo").owlCarousel({
-		autoPlay: 70000,
+		autoPlay: 50000,
 		items : 6,
 		itemsDesktop : [1199,5],
 		itemsDesktopSmall : [979,5],
 	});
 
 	$("#works-logo").owlCarousel({
-		autoPlay: 70000,
+		autoPlay: 50000,
 		items : 6,
 		itemsDesktop : [1199,5],
 		itemsDesktopSmall : [979,5],
